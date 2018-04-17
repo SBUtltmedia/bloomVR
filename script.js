1a227bd0264e611127a03cba49e0d02d610f13cc
Manpreet Singh <nhpmanpreet@gmail.com>
2018-04-17 14:03:20 -0400

    Reformat for 0.8.2

--- script.js
+++ script.js
@@ -4,15 +4,24 @@ var terrainWidth = 7;
 var terrainLength = 5;
 var colorScaleWidth = 255;
 var data;
-var colWords = ["factual","conceptual", "procedural", "metacognitive"]
-var rowWords = ['remember',"understand","apply","analyze","evaluate","create"]
+var colWords = ["factual", "conceptual", "procedural", "metacognitive"]
+var rowWords = ['remember', "understand", "apply", "analyze", "evaluate", "create"]
+
+
+AFRAME.components['look-controls'].Component.prototype.tick = () => {}
+
+
 
 $.when(getBloomData()).then(
     function (bloomData) {
         console.log(bloomData)
         data = bloomData;
         var sceneEl = document.querySelector('a-scene');
+        var smartText = sceneEl.querySelector('#textHolder');
         var stepHolder = document.querySelector('#stepHolder');
+        smartText.emit("textHide")
+        var camera = sceneEl.querySelector('a-camera');
+        var sky = sceneEl.querySelector('a-sky');
         if (sceneEl.hasLoaded) {
             run();
         } else {
@@ -20,6 +29,12 @@ $.when(getBloomData()).then(
                 passive: true
             });
         }
+
+        //        $(sky).addEventListener('mouseenter',function (evt) {
+        //            smartText.emit('textHide')
+        //            console.log("fuse")
+        //        })
+
 
         function run() {
             var hue = 250;
@@ -47,62 +62,72 @@ $.when(getBloomData()).then(
                     if (y == 0 && x == 0) {
                         continue;
                     }
-                    var shrinkage = ((lengthCounter-1) + (widthCounter-1)) * shrinkScale;
+                    var shrinkage = ((lengthCounter - 1) + (widthCounter - 1)) * shrinkScale;
                     var height;
                     var text = "";
                     var color = ""
                     if (x == 0) {
                         height = 0.01;
-                        text = colWords[y-1]
+                        text = colWords[y - 1]
                         color = "white";
-                    }
-                    else if (y == 0) {
+
+                    } else if (y == 0) {
                         height = 0.01;
-                        text = rowWords[x-1]
+                        text = rowWords[x - 1]
                         color = "red";
-                    }
-                    else {
+                    } else {
                         height = shrinkage;
                     }
-                    
-                    
-
 
                     brightnessTemp -= 10;
                     var entityEl = document.createElement('a-entity');
-                             var smartText = sceneEl.querySelector('#textHolder');
+                    var smartText = sceneEl.querySelector('#textHolder');
                     entityEl.setAttribute("id", `${(y)}_${(x)}`);
                     entityEl.setAttribute("step", "color: hsl(" + hue + ", " + saturation + "% ," + brightnessTemp + "%)");
                     entityEl.setAttribute("step", "height: " + height);
                     entityEl.setAttribute("class", "step");
-                    entityEl.setAttribute("text", "value: " + text + "; width: 3; align:center; baseline: bottom; color: " + color);
+                    var label = document.createElement('a-entity')
+                    label.setAttribute("text" , "value: " + text + "; width: 3; align:center; baseline: bottom; color: " + color);
+                    label.setAttribute("rotation", "-20 45 0")
+                    console.log(text)
+                    entityEl.append(label)
                     entityEl.setAttribute("position", `${-x} ${height/2} ${-y}  `);
                     countShiftY++
                     stepHolder.appendChild(entityEl);
-                    
-                   $(entityEl).on("mouseleave",function(evt){      smartText.emit('textHide')})
-                    
-                    $(entityEl).on("fusing", function (evt) {
+
+
+                    entityEl.addEventListener("mouseenter", function (evt) {
                         var id = evt.currentTarget.id
                         var [row, col] = id.split("_")
-                        console.log(row,col)
-                        var title= document.querySelector("#textHolder #title");
-                        var body= document.querySelector("#textHolder #body");
-                        var [titleText,bodyText]=data[row][col].split("|");
-                       // text="color: red;align:center;whiteSpace:pre;"
-                        
-                            title.setAttribute("text", {
-                            value: titleText ,color: 'red',align:'center',whiteSpace:'pre'
+                        console.log(row, col)
+                        var title = document.querySelector("#textHolder #title");
+                        var body = document.querySelector("#textHolder #body");
+                        var [titleText, bodyText] = data[row][col].split("|");
+                        // text="color: red;align:center;whiteSpace:pre;"
+
+                        title.setAttribute("text", {
+                            value: titleText,
+                            color: 'red',
+                            align: 'center',
+                            whiteSpace: 'pre'
                         });
-                        
+
                         body.setAttribute("text", {
-                            value: '\n' + bodyText + '\n\n',color: 'blue',align:'center',whiteSpace:'pre'
+                            value: bodyText + '\n\n',
+                            color: 'blue',
+                            align: 'center',
+                            whiteSpace: 'pre'
                         });
-                        
-                        
-                        var smartText = sceneEl.querySelector('#textHolder');
+
+
+
                         smartText.emit('textShow')
+
+
+
                     });
+
+
                     lengthCounter++;
                 }
                 widthCounter++;
--- step.js
+++ step.js
@@ -19,39 +19,55 @@ AFRAME.registerComponent('step', {
         },
         opacity: {
             type: 'number',
-            default: 0.5
-        },
-        text: {
-            type: 'string',
-            default: 'test'
+            default: 1.0
         },
     },
 
     init: function () {
         var data = this.data;
+        console.log(data)
         var el = document.createElement('a-entity');
-        var el2 =document.createElement('a-entity');
+        var el2 = document.createElement('a-entity');
+        var label = document.createElement('a-entity');
+
+        
+
+        
+
         var geometry = new THREE.BoxBufferGeometry(data.width, data.height, data.depth);
         var position = data.position;
         var material = new THREE.MeshStandardMaterial({
             color: data.color,
-            
+
         });
-        
+
         var material2 = new THREE.MeshStandardMaterial({
-            
+
         });
-        
+
         var mesh1 = new THREE.Mesh(geometry, material);
         el.setObject3D('mesh', mesh1);
         var mesh2 = new THREE.Mesh(geometry, material2);
-        el2.setObject3D('mesh',mesh2)
+        el2.setObject3D('mesh', mesh2)
         el2.setAttribute("material", "src: border.png; transparent:true");
+
+        this.el.append(el);
+        this.el.append(el2);
+        //this.el.append(label);
         
-      this.el.append(el);
-      this.el.append(el2);
-        
-        
+        console.log(data.label)
+        var sceneEl = document.querySelector('a-scene');
+        var smartText = sceneEl.querySelector('#textHolder');
+
+        this.el.addEventListener("mouseleave", function (evt) {
+            setTimeout(function () {
+                smartText.emit('textHide')
+                console.log("step")
+            }, 200);
+
+        })
+
+
     },
 
     /**
@@ -60,7 +76,7 @@ AFRAME.registerComponent('step', {
     update: function (oldData) {
         var data = this.data;
         var el = this.el;
-
+//        el.querySelector('.label').setAttribute('rotation', {x:document.querySelector('a-camera').getAttribute("rotation").x,y:document.querySelector('a-camera').getAttribute("rotation").y,z:document.querySelector('a-camera').getAttribute("rotation").z})
         // If `oldData` is empty, then this means we're in the initialization process.
         // No need to update.
         if (Object.keys(oldData).length === 0) {
